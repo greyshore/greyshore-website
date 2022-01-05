8517d043d981610c0531a3f0bd29ecc17186d760
Louise Clark <louise@consideringA11y.com>
2022-01-05 11:55:12 -0500

    adds image plugin for social

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,43 @@
 const htmlmin = require("html-minifier");
 const markdownIt = require('markdown-it');
 const pluginRss = require("@11ty/eleventy-plugin-rss");
+const Image = require("@11ty/eleventy-img");
+
+// Image shortcode for social sharing
+async function imageShortcode(src, alt, sizes) {
+  let metadata = await Image(src, {
+    widths: [null],
+    formats: ["png"]
+  });
+
+  let imageAttributes = {
+    alt: "",
+    sizes,
+    loading: "lazy",
+    decoding: "async",
+  };
+
+  (async () => {
+    let url = "src/static/img/social.png";
+    let stats = await Image(url);
+  
+    console.log( stats );
+  })();
+
+  // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+  return Image.generateHTML(metadata, imageAttributes, {
+    whitespaceMode: "inline"
+  });
+}
 
 module.exports = function (eleventyConfig) {
   // PLUGINS
   eleventyConfig.addPlugin(pluginRss);
+
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
+  eleventyConfig.addLiquidShortcode("image", imageShortcode);
+  eleventyConfig.addJavaScriptFunction("image", imageShortcode);
+  
 
   // shortcode to render markdown from string => {{ STRING | markdown | safe }}
   eleventyConfig.addFilter('markdown', function(value) {
@@ -13,6 +46,8 @@ module.exports = function (eleventyConfig) {
     });
     return markdown.render(value);
   });
+
+
 
   // Copy `img/favicon/` to `_site/`
   eleventyConfig.addPassthroughCopy({ "src/static/img/favicon": "/" });
